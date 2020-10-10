7603b830a45c4471d2a8f6e6eefb69fcf39d7bbe
rishabhgawai123 <rishabhgawai123@gmail.com>
2020-10-10 11:08:49 +0530

    Updates

---
+++ precache-manifest.27b594f8a3d22320240d1bfd7ee1d1bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803ff622f1f950418c936b5d86ce09ec",
+    "url": "/Covidtracker/index.html"
+  },
+  {
+    "revision": "71d9e90afaed551f5a8f",
+    "url": "/Covidtracker/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c08cfb5f1dbf1a036ad1",
+    "url": "/Covidtracker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "71d9e90afaed551f5a8f",
+    "url": "/Covidtracker/static/js/2.101f9cf5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Covidtracker/static/js/2.101f9cf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c08cfb5f1dbf1a036ad1",
+    "url": "/Covidtracker/static/js/main.5d5b04cb.chunk.js"
+  },
+  {
+    "revision": "e2275623e89c611cd8ed",
+    "url": "/Covidtracker/static/js/runtime-main.9f0a6b7b.js"
+  }
+]);
